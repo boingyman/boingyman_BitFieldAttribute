5958fe40052982994aa0339bd086d36d38c83bde
boingyman <poolokwu@hotmail.com>
2017-05-18 20:54:10 -0500

    Initial commit

---
+++ Bit Field Attribute/BitFieldAttribute.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using System;
+using UnityEngine;
+
+public class BitFieldAttribute : PropertyAttribute {
+
+	public readonly string[] valueList;
+	
+	public BitFieldAttribute(Type enumType) {
+		valueList = Enum.GetNames(enumType);
+	}
+
+	public BitFieldAttribute(string[] names) {
+		valueList = names;
+	}
+}
---
+++ Bit Field Attribute/Editor/BitFieldDrawer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static partial class PropertyDrawers {
+
+	[CustomPropertyDrawer(typeof(BitFieldAttribute))]
+	public class BitFieldDrawer : PropertyDrawer {
+
+		private BitFieldAttribute _attributeValue = null;
+		private BitFieldAttribute attributeValue {
+			get {
+				if (_attributeValue == null) _attributeValue = (BitFieldAttribute)attribute;
+				return _attributeValue;
+			}
+		}
+
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+			label = EditorGUI.BeginProperty(position, label, property);
+			Rect contentPosition = EditorGUI.PrefixLabel(position, label);
+
+			property.intValue = EditorGUI.MaskField(contentPosition, property.intValue, attributeValue.valueList);
+			EditorGUI.EndProperty();
+		}
+
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+			return base.GetPropertyHeight(property, label);
+		}
+	}
+
+}
